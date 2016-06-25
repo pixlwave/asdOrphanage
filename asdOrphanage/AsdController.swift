7908d3417d02bd739f6de3d6e0cb270af721d271
Doug <git@pixlwave.uk>
2016-06-25 10:20:33 +0100

    Convert to Swift 4.0

--- asdOrphanage/AsdController.swift
+++ asdOrphanage/AsdController.swift
@@ -6,24 +6,17 @@ class AsdController: NSViewController {
 
     @IBOutlet weak var directoryField: NSTextField!
     @IBOutlet weak var orphansTable: NSTableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        orphansTable.doubleAction = "showOrphan:"
-        orphansTable.target = self
-    }
 
-    @IBAction func analyse(sender: NSButton) {
+    @IBAction func analyse(_ sender: NSButton) {
         let selectDirectory = NSOpenPanel()
         var directory = ""
         
         selectDirectory.canChooseDirectories = true
         selectDirectory.canChooseFiles = false
         
-        if selectDirectory.runModal() == NSFileHandlingPanelOKButton {
-            let directoryURL = selectDirectory.URLs[0] 
-            directory = directoryURL.path!
+        if selectDirectory.runModal() == .OK {
+            let directoryURL = selectDirectory.urls[0] 
+            directory = directoryURL.path
             directoryField.stringValue = directory
             orphans = AsdOrphans.analyse(directory)
         }
@@ -31,11 +24,11 @@ class AsdController: NSViewController {
         orphansTable.reloadData()
     }
     
-    func showOrphan(sender: NSTableView) {
+    @IBAction func showOrphan(_ sender: NSTableView) {
         if orphansTable.clickedRow != -1 {
-            var file = orphans[orphansTable.clickedRow]
-            file = directoryField.stringValue.stringByAppendingPathComponent(file)
-            AsdOrphans.showFile(file)
+            let file = orphans[orphansTable.clickedRow]
+            let url = URL(fileURLWithPath: file, relativeTo: URL(fileURLWithPath: directoryField.stringValue))
+            AsdOrphans.showFile(url)
         }
     }
     
@@ -44,12 +37,12 @@ class AsdController: NSViewController {
 // MARK: NSTableViewDataSource
 extension AsdController: NSTableViewDataSource {
     
-    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+    func numberOfRows(in tableView: NSTableView) -> Int {
         return orphans.count
     }
     
-    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row rowIndex: Int) -> AnyObject? {
-        return orphans[rowIndex]
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        return orphans[row]
     }
     
 }
--- asdOrphanage/AsdOrphans.swift
+++ asdOrphanage/AsdOrphans.swift
@@ -2,28 +2,30 @@ import AppKit
 
 class AsdOrphans {
     
-    class func analyse(directory: String) -> [String] {
+    class func analyse(_ directory: String) -> [String] {
         var asdFiles = [String]()
         var asdOrphans = [String]()
         
-        if let fileList = NSFileManager.defaultManager().subpathsAtPath(directory) {
+        if let fileList = FileManager.default.subpaths(atPath: directory) {
             
             for file in fileList {
                 if file.hasSuffix(".asd") { asdFiles.append(file) }
             }
             
             for asdFile in asdFiles {
-                let asdParentFile = asdFile.substringToIndex(asdFile.endIndex.advancedBy(-4))
-                let asdParentFilePath = directory.stringByAppendingPathComponent(asdParentFile)
-                if NSFileManager.defaultManager().fileExistsAtPath(asdParentFilePath) == false { asdOrphans.append(asdFile) }
+                let asdParentFile = String(asdFile[..<asdFile.index(asdFile.endIndex, offsetBy: -4)])
+                let asdParentFilePath = URL(fileURLWithPath: asdParentFile, relativeTo: URL(fileURLWithPath: directory)).path
+                if FileManager.default.fileExists(atPath: asdParentFilePath) == false {
+                    asdOrphans.append(asdFile)
+                }
             }
         }
         
         return asdOrphans
     }
     
-    class func showFile(file: String) {
-        NSWorkspace.sharedWorkspace().selectFile(file, inFileViewerRootedAtPath: "")
+    class func showFile(_ url: URL) {
+        NSWorkspace.shared.selectFile(url.path, inFileViewerRootedAtPath: "")
     }
     
 }
